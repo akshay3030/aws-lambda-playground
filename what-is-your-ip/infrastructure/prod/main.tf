da8ebf0c9fad369ed2e54ff1d3d8fb9b984a7fed
akumar5 <akshay.kumar@elliemae.com>
2019-01-29 15:34:12 -0800

    changes to create aws lambda fn along with other resources, use api gateway original endpoint to get the public ip

--- what-is-your-ip/infrastructure/prod/main.tf
+++ what-is-your-ip/infrastructure/prod/main.tf
@@ -1,3 +1,78 @@
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "what_is_your_ip_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  filename         = "../../functions/what_is_your_ip/main.zip"
+  function_name    = "what-is-your-ip"
+  role             = "${aws_iam_role.iam_for_lambda.arn}"
+  handler          = "main.handle"
+  source_code_hash = "${base64sha256(file("../../functions/what_is_your_ip/main.zip"))}"
+  runtime          = "python2.7"
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
+}
+
+
+# This is to optionally manage the CloudWatch Log Group for the Lambda Function.
+# If skipping this resource configuration, also add "logs:CreateLogGroup" to the IAM policy below.
+resource "aws_cloudwatch_log_group" "example" {
+  name              = "/aws/lambda/${aws_lambda_function.test_lambda.function_name}"
+  retention_in_days = 14
+}
+
+# See also the following AWS managed policy: AWSLambdaBasicExecutionRole
+resource "aws_iam_policy" "lambda_logging" {
+  name = "lambda_logging"
+  path = "/"
+  description = "IAM policy for logging from a lambda"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role = "${aws_iam_role.iam_for_lambda.name}"
+  policy_arn = "${aws_iam_policy.lambda_logging.arn}"
+}
+
 module "iam" {
   source = "../modules/iam"
 
@@ -9,7 +84,7 @@ module "api_gateway" {
 
   name       = "${var.name}"
   aws_region = "${var.aws_region}"
-  lambda_arn = "${var.apex_function_what_is_your_ip}"
+  lambda_arn = "${aws_lambda_function.test_lambda.arn}"
 }
 
 module "cloudfront" {
--- what-is-your-ip/infrastructure/prod/variables.tf
+++ what-is-your-ip/infrastructure/prod/variables.tf
@@ -2,9 +2,11 @@ variable "name" {
   default = "what-is-your-ip"
 }
 
-variable "aws_region" {}
+variable "aws_region" {
+  default = "us-west-2"
+}
 
-variable "apex_function_what_is_your_ip" {}
+#variable "apex_function_what_is_your_ip" {}
 
 variable "cf_config" {
   default = {
